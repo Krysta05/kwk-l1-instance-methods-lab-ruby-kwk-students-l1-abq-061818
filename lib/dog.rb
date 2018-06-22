83bc6e6ddfd75aad6bba47d6cbd4242e89026046
Krysta05 <martinezkrysta7@gmail.com>
2018-06-22 20:07:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,11 @@
 Class Dog 
 
-def Bark '=' 'Wolf'
+def Bark = 'Wolf'
   @Bark
 puts 'Wolf'
 end
 
-def Sits '=' 'Dog is sitting'
+def Sits = 'Dog is sitting'
   @Sits 
 puts 'Dog is sitting'
 end 
