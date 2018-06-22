cd5e456072ebec381902a3c774961b9834c2d0a8
Krysta05 <martinezkrysta7@gmail.com>
2018-06-22 20:19:41 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,11 @@
 Class Person
 
 def Talk = 'Hello there'
-  @Talk
-puts 'Hello there'
+ puts 'Hello there'
 end 
 
 def Walk = 'The person is walking'
-  @walk 
-puts 'The person is walking'
+ puts 'The person is walking'
 end  
 
  end 
